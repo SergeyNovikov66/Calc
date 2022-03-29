ea1be641982dfcad02e9191f97aca8318dd6519d
Sergey Novikov <snovikov196605@gmail.com>
2022-03-29 12:42:18 +0700

    first

---
+++ calc.js
@@ -0,0 +1,93 @@
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/', '%', '+/-'];
+
+const out = document.querySelector('.calc-screen p');
+
+function clearAll () {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (Event) => {
+    // нажата не кнопка
+    if(!Event.target.classList.contains('btn')) return;
+    // нажата кнопка clearAll
+    if(Event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    // получаю нажатую кнопку
+    const key = Event.target.textContent;
+
+    // если нажата клавиша 0-9
+    if (digit.includes(key)) {
+        if (b === '' && sign === '') {
+            a += key;
+           
+            out.textContent = a;
+        }
+        else if (a!=='' && b!=='' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b, sign);
+        return;
+    }
+
+    // если нажата клавиша +-.*
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.log(a, b, sign);
+        return;
+    }
+
+    //нажата клавиша =
+    if(key === '=') {
+        if (b ==='') b = a;
+        switch (sign) {
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                 a = a - b;
+                break;
+            case "X":
+                 a = a * b;
+                break;
+            case "/":
+                if (b === '0') {
+                    out.textContent = 'Ошибка';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = a / b;
+                break;
+            case "%":
+                a = (b/100) * a;
+                break;
+            case "+/-":
+                a = -a;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.log(a, b, sign);
+    }
+}
